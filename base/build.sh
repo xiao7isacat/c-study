0385c93f507b74bfc5d8af9576bdeb097c2729c3
wangtangyu <xiao7isacat@gmail.com>
2024-11-07 15:50:14 +0800

    sport jump  definition

--- base/build.sh
+++ base/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rm -rf ./_builds
-cmake -S . -DCMAKE_BUILD_TYPE=Debug -B _builds
+rm -rf ./compile_commands.json
+cmake -S . -DCMAKE_BUILD_TYPE=Debug -B _builds -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 #cmake -S . -DCMAKE_BUILD_TYPE=Release -B _builds
+ln -s ./_builds/compile_commands.json ./
 cd _builds
 make
